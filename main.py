3c309b1c37b92258139e99b850c78bc0bee279f0
MiggyJ <miggyj9@gmail.com>
2021-08-09 20:49:20 +0800

    fixed table generation w/ pure SQLAlchemy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from routes import userRoutes
+from routes import userRoutes, postRoutes
+
+from database import Base, engine
+
+#* Generate (Automatic) Tables from SQLAlchemy models
+#* Must drop tables for updates
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(userRoutes.router)
--- models/userModel.py
+++ models/userModel.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, DateTime, text
 from sqlalchemy.sql.schema import Column
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String(255), nullable=False)
     age = Column(Integer, nullable=False)
+    created_at = Column(DateTime, server_default=text('NOW()'))
+    updated_at = Column(DateTime, server_onupdate=text('NOW()'))
+
+    posts = relationship('posts', back_populates='author')
